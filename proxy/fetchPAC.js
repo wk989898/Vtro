cc9766024a8be92c2b1cc6a166b81bbf00ecb253
wk989898 <wangkun989898@gmail.com>
2020-05-20 22:44:22 +0800

    fetchPAC

---
+++ proxy/fetchPAC.js
@@ -0,0 +1,21 @@
+/**
+ * gfwlist2pac
+ * Automatically convert gfwlist to pac everyday
+ * Just use https://raw.githubusercontent.com/petronny/gfwlist2pac/master/gfwlist.pac
+ * 
+ */
+const https = require('https')
+const fs = require('fs')
+export function fetchPAC(path) {
+  const writerStream = fs.createWriteStream(path)
+  https.get('https://raw.githubusercontent.com/petronny/gfwlist2pac/master/gfwlist.pac')
+    .on('response', r => {
+      r.on('data', res => {
+        writerStream.write(res);
+      })
+    }).end(() => {
+      writerStream.end();
+    })
+}
+fetchPAC('./pac.js')
+console.log('write into pac.js')
