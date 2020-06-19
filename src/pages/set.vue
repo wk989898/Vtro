75e9bb9479a44736d057a2b6ed91e211422a5cc9
wk989898 <wangkun989898@gmail.com>
2020-06-19 19:47:57 +0800

    optimize timejs

--- src/pages/set.vue
+++ src/pages/set.vue
@@ -7,16 +7,29 @@
         <el-radio v-model="proxy" label="off">关闭代理</el-radio>
       </el-tab-pane>
       <el-tab-pane label="时间分流(夜间节点)">
-        <el-time-select placeholder="起始时间" v-model="startTime" :picker-options="{start: '00:00', step: '00:15', end: '23:45'}">
+        <el-time-select
+          placeholder="起始时间"
+          v-model="startTime"
+          :picker-options="{ start: '00:00', step: '00:15', end: '23:45' }"
+        >
         </el-time-select>
-        <el-time-select placeholder="结束时间" v-model="endTime" :picker-options="{ start: '00:00',step: '00:15',end: '23:45'}">
+        <el-time-select
+          placeholder="结束时间"
+          v-model="endTime"
+          :picker-options="{ start: '00:00', step: '00:15', end: '23:45' }"
+        >
         </el-time-select>
         <el-button @click="selectNode">更改</el-button>
-        <p>夜间节点：{{night?night.name:'暂无设置'}}</p>
+        <p>夜间节点：{{ night ? night.name : "暂无设置" }}</p>
       </el-tab-pane>
       <el-tab-pane label="开机启动">
         <label>开机启动：</label>
-        <el-switch v-model="login" active-color="#13ce66" inactive-color="#ff4949" @change="changeLogin" />
+        <el-switch
+          v-model="login"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+          @change="changeLogin"
+        />
         <p>可能会不生效~</p>
       </el-tab-pane>
       <el-tab-pane label="监听端口" class="listen">
@@ -29,10 +42,33 @@
         <el-button @click="portReset">更改</el-button>
       </el-tab-pane>
       <el-tab-pane label="其他">
-        <el-switch style="display: block" v-model="isIP" @change="setOther('isIP')" active-color="#f1ff11" inactive-color="#114949" active-text="ip连接" inactive-text="域名连接" /><br/> reuse session:
-        <el-switch v-model="reuse_session" label="reuse_session" @change="setOther('reuse_session')" /><br/><br/> reuse port:
-        <el-switch v-model="reuse_port" label="reuse_port" @change="setOther('reuse_port')" /><br/><br/> fast open:
-        <el-switch v-model="fast_open" label="fast_open" @change="setOther('fast_open')" /><br/><br/>
+        <el-switch
+          style="display: block"
+          v-model="isIP"
+          @change="setOther('isIP')"
+          active-color="#f1ff11"
+          inactive-color="#114949"
+          active-text="ip连接"
+          inactive-text="域名连接"
+        /><br />
+        reuse session:
+        <el-switch
+          v-model="reuse_session"
+          label="reuse_session"
+          @change="setOther('reuse_session')"
+        /><br /><br />
+        reuse port:
+        <el-switch
+          v-model="reuse_port"
+          label="reuse_port"
+          @change="setOther('reuse_port')"
+        /><br /><br />
+        fast open:
+        <el-switch
+          v-model="fast_open"
+          label="fast_open"
+          @change="setOther('fast_open')"
+        /><br /><br />
         <span>如果不懂请保持默认~</span>
       </el-tab-pane>
     </el-tabs>
@@ -40,43 +76,37 @@
 </template>
 
 <script>
-  import {
-    calcTime
-  } from '../utils/time'
-  import {
-    clearTimeout
-  } from 'timers';
-  export default {
-    data() {
-      return {
-        tabPosition: 'right',
-        mode: null,
-        login: false,
-        proxy: null,
-        startTime: null,
-        endTime: null,
-        night: null,
-        pid1: null,
-        pid2: null,
-        listen: [],
-        isIP: true,
-        reuse_port: false,
-        fast_open: false,
-        reuse_session: true
-      }
-    },
-    mounted() {
-      ipc.send('get-login')
-      ipc.send('getConf')
-      ipc.on('config', (e, conf) => {
+import { calcTime } from "../utils/time"
+import { clearTimeout } from "timers"
+export default {
+  data() {
+    return {
+      tabPosition: "right",
+      mode: null,
+      login: false,
+      proxy: null,
+      startTime: null,
+      endTime: null,
+      night: null,
+      pid1: null,
+      pid2: null,
+      listen: [],
+      isIP: true,
+      reuse_port: false,
+      fast_open: false,
+      reuse_session: true
+    }
+  },
+  mounted() {
+    ipc.send("get-login")
+    ipc.send("getConf")
+    ipc
+      .on("config", (e, conf) => {
         let {
           proxy,
           night,
           mode,
-          time: {
-            startTime,
-            endTime
-          },
+          time: { startTime, endTime },
           listen
         } = conf
         this.proxy = proxy
@@ -85,99 +115,106 @@
         this.night = night
         this.mode = mode
         this.listen = listen
-      }).on('login', (e, login) => {
-        this.login = login
-      }).on('update-mode', () => {
-        ipc.send('getConf')
       })
-      setTimeout(() => {
-        this.openNight({
-          startTime: this.startTime,
-          endTime: this.endTime
-        })
-      }, 1000)
+      .on("login", (e, login) => {
+        this.login = login
+      })
+      .on("update-mode", () => {
+        ipc.send("getConf")
+      })
+    setTimeout(() => {
+      this.openNight({
+        startTime: this.startTime,
+        endTime: this.endTime
+      })
+    }, 1000)
+  },
+  watch: {
+    proxy: function(newval, old) {
+      if (!old) return
+      ipc.send("setConf", {
+        proxy: newval
+      })
+    }
+  },
+  methods: {
+    changeLogin(e) {
+      ipc.send("set-login", e)
     },
-    watch: {
-      proxy: function(newval, old) {
-        if (!old) return;
-        ipc.send('setConf', {
-          proxy: newval
-        })
+    selectNode() {
+      if (this.pid1) {
+        window.clearTimeout(this.pid1)
+        this.pid1 = null
       }
+      if (this.pid2) {
+        window.clearTimeout(this.pid2)
+        this.pid2 = null
+      }
+      const time = {
+        startTime: this.startTime,
+        endTime: this.endTime
+      }
+      ipc.send("setConf", {
+        time
+      })
+      if (!this.night) {
+        return this.$message("请确认是否有夜间节点~")
+      }
+      const tem = this.openNight(time)
+      tem && this.$message("更改成功")
     },
-    methods: {
-      changeLogin(e) {
-        ipc.send('set-login', e)
-      },
-      selectNode() {
-        if (this.pid1) {
-          window.clearTimeout(this.pid1)
-          this.pid1 = null
-        }
-        if (this.pid2) {
-          window.clearTimeout(this.pid2)
-          this.pid2 = null
-        }
-        const time = {
-          startTime: this.startTime,
-          endTime: this.endTime
-        }
-        ipc.send('setConf', {
-          time
-        })
-        if (!this.night) {
-          return this.$message('请确认是否有夜间节点~')
-        }
-        const tem = this.openNight(time)
-        tem && this.$message('更改成功')
-      },
-      timer(mode, timeout, cb) {
-        return window.setTimeout(() => {
-          ipc.send('change-mode', mode)
-        }, timeout);
-      },
-      openNight(time) {
-        if (this.pid1 || this.pid2) return false;
-        const [isOpen, last, night = 0] = calcTime(time)
-        if (last === 0) {
-          this.$message('设置夜间节点无效')
-          return false
-        }
-        console.log('time.js', isOpen, last, night)
-        night && this.mode === 'night' && this.timer('day', 0)
-        this.pid1 = this.timer('night', night * 1000)
-        this.pid2 = this.timer('day', last * 1000)
-        return true
-      },
-      portReset() {
-        const max = 1 << 16
-        let isPort = false
-        const listen = this.listen.map((v, i) => {
-          v = Number(v)
-          if (v >= max || v <= 0) {
-            this.$message('无效的端口')
-            isPort = true
-            return [1080, 1081, 1082][i]
-          }
-          return v
-        })
-        if (isPort) return;
-        ipc.send('setConf', {
-          listen
-        })
-      },
-      setOther(type) {
-        ipc.send('other', [type, this[type]])
+    timer(mode, timeout, cb) {
+      return window.setTimeout(() => {
+        ipc.send("change-mode", mode)
+      }, timeout)
+    },
+    openNight(time) {
+      if (this.pid1 || this.pid2) return false
+      const [isOpen, last, night = 0] = calcTime(time)
+      if (last === 0) {
+        this.$message("设置夜间节点无效")
+        return false
       }
+      console.log(
+        "time.js %s start %d before %d",
+        isOpen,
+        last * 60,
+        night * 60
+      )
+      night && this.mode === "night" && this.timer("day", 0)
+      this.pid1 = this.timer("night", night * 1000 * 60)
+      this.pid2 = this.timer("day", last * 1000 * 60)
+      return true
+    },
+    portReset() {
+      const max = 1 << 16
+      let isPort = false
+      const listen = this.listen.map((v, i) => {
+        v = Number(v)
+        if (v >= max || v <= 0) {
+          this.$message("无效的端口")
+          isPort = true
+          return [1080, 1081, 1082][i]
+        }
+        return v
+      })
+      if (isPort) return
+      ipc.send("setConf", {
+        listen
+      })
+    },
+    setOther(type) {
+      ipc.send("other", [type, this[type]])
     }
   }
+}
 </script>
 <style scoped>
-  .listen {
-    font-size: 14px;
-    margin: 0 0 30px 10px;
-  }
-  .listen button {
-    margin-top: 3px;
-  }
+.listen {
+  font-size: 14px;
+  margin: 0 0 30px 10px;
+}
+.listen button {
+  margin-top: 3px;
+}
 </style>
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,30 +1,47 @@
 export function calcTime({ startTime, endTime }) {
-  const start = startTime
-  const end = endTime
-  if (start === end) return [false, 0]
+  // const start = startTime
+  // const end = endTime
+  if (startTime === endTime) return [false, 0]
   const now = new Date()
-  var [starthour, startmin] = start.split(':').map(v => Number(v))
-  var [endhour, endmin] = end.split(':').map(v => Number(v))
+  var [starthour, startmin] = startTime.split(':').map(v => Number(v))
+  var [endhour, endmin] = endTime.split(':').map(v => Number(v))
   var [hour, min] = [Number(now.getHours()), Number(now.getMinutes())]
-
-  endhour = endhour > starthour ? endhour :
-    endhour < starthour ? endhour + 24 :
-      // hour 相同  17:30 -> 17:00
-      endmin > startmin ? endhour : endhour + 24
-  if ((hour > starthour || (hour == starthour && min > startmin)) &&
-    (hour < endhour || (hour == endhour && endmin < min))
-  ) {
-    // on 剩余时间
-    console.log('on')
-    let last = ((endhour * 60 + endmin) - (hour * 60 + min)) * 60
-    return [true, last]
-  } else {
-    // off bt后开启 last 持续时间
-    console.log('off')
-    let bt = ((starthour * 60 + startmin) + (hour * 60 + min)) * 60
-    let last = ((endhour * 60 + endmin) - (starthour * 60 + startmin)) * 60
-    return [false, last === 0 ? 0 : bt + last, bt]
+  /**
+   * rewrite
+   */
+  const start=starthour*60+startmin
+  const end=endhour*60+endmin
+  const _now=hour*60+min
+  if(start>end){
+    // 隔夜
+    if(_now>start||_now<end){
+      const last=_now>start?(_start-last+_end):(end-_now)
+      return [true,last]
+    }else return [false,end+24*60-start,start-_now]
+  }else {
+    if(_now<start){
+      return [false,start-_now,end-start]
+    }else return [true,end-_now]
   }
+
+  // endhour = endhour > starthour ? endhour :
+  //   endhour < starthour ? endhour + 24 :
+  //     // hour 相同  17:30 -> 17:00
+  //     endmin > startmin ? endhour : endhour + 24
+  // if ((hour > starthour || (hour == starthour && min > startmin)) &&
+  //   (hour < endhour || (hour == endhour && endmin < min))
+  // ) {
+  //   // on 剩余时间
+  //   console.log('on')
+  //   let last = ((endhour * 60 + endmin) - (hour * 60 + min)) * 60
+  //   return [true, last]
+  // } else {
+  //   // off bt后开启 last 持续时间
+  //   console.log('off')
+  //   let bt = ((starthour * 60 + startmin) + (hour * 60 + min)) * 60
+  //   let last = ((endhour * 60 + endmin) - (starthour * 60 + startmin)) * 60
+  //   return [false, last === 0 ? 0 : bt + last, bt]
+  // }
 }
 /**
  * @returns {string}   e.g. y-m-d h-m-s
